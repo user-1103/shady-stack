ec82d65f572e149fd8514734e7ab9f0ff3518766
USER 1103 <79700949+user-1103@users.noreply.github.com>
2023-03-01 19:28:24 -0700

    addding build deps

--- flake.nix
+++ flake.nix
@@ -22,7 +22,7 @@
               pytest = super.pytest.overridePythonAttrs
                 (
                   old: {
-                    buildInputs = (old.buildInputs or [ ]) ++ [ super.hatchling ];
+                    buildInputs = (old.buildInputs or [ ]) ++ [ super.hatch ];
                   }
                 );
             });

03f38dd2429da39ddc13526ec360572f09d08d15
USER 1103 <79700949+user-1103@users.noreply.github.com>
2023-03-03 17:35:50 -0700

    testing discord bridge

--- shadybackend/demon.py
+++ shadybackend/demon.py
@@ -19,25 +19,28 @@ def run_api_demon(root: Path, api_paths: List[Path]) -> None:
     call_hooks(HookTypes.INIT)
     for path in api_paths:
         collect_apis(path)
-    while (G["run"]):
-        if (len(G["Q"])):
-            call_hooks(HookTypes.PRE)
-            req = G["Q"].pop()
-            try:
-                process_request(req)
-            except Exception as e:
-                call_hooks(HookTypes.ERR)
-                err = e
-                log.error(f"Failed to process {req}: {e}")
+    try:
+        while (G["run"]):
+            if (len(G["Q"])):
+                call_hooks(HookTypes.PRE)
+                req = G["Q"].pop()
+                try:
+                    process_request(req)
+                except Exception as e:
+                    call_hooks(HookTypes.ERR)
+                    err = e
+                    log.error(f"Failed to process {req}: {e}")
+                else:
+                    call_hooks(HookTypes.OK)
+                finally:
+                    call_hooks(HookTypes.POST)
             else:
-                call_hooks(HookTypes.OK)
-            finally:
-                call_hooks(HookTypes.FATAL)
-                log.fatal("Unable to deal with err")
-            call_hooks(HookTypes.POST)
-        else:
-            call_hooks(HookTypes.WAIT)
-            sleep(MAX_SLEEP)
-    call_hooks(HookTypes.EXIT)
+                call_hooks(HookTypes.WAIT)
+                sleep(MAX_SLEEP)
+        call_hooks(HookTypes.EXIT)
+    except Exception as e:
+        log.fatal(f"Fatal err: {e}")
+        call_hooks(HookTypes.FATAL)
+
 
 log.debug("Loaded demon lib.")

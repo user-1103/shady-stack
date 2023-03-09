b721d046f4e499162beb2088ab3e83fdae0e25c8
USER 1103 <79700949+user-1103@users.noreply.github.com>
2023-03-09 14:26:52 -0700

    fixing the brisdge

--- shadybackend/bridges.py
+++ shadybackend/bridges.py
@@ -4,6 +4,11 @@ A central module to look up the available backends.
 
 from dataclasses import dataclass
 from typing import Callable, Dict
+import discord
+from typing import Any, Dict
+from shadybackend.request_tools import Request
+from json import loads
+import logging as log
 
 BRIDGES: Dict[str, 'Bridge'] = dict()
 
@@ -24,5 +29,33 @@ class Bridge():
         """
         BRIDGES.update({self.name: self})
 
-# Discord Bridge
-import discord_bridge
+class DiscordBridge(Bridge):
+    """
+    This is just the example code from:
+    https://discordpy.readthedocs.io/en/stable/quickstart.html
+    """
+    name = "discord"
+
+    def build_bridge(self, g: Dict[str, Any]) -> None:
+        intents = discord.Intents.default()
+        intents.message_content = True
+
+        client = discord.Client(intents=intents)
+
+        @client.event
+        async def on_ready():
+            log.info(f'Started Discord Bridge {client.user}')
+
+        @client.event
+        async def on_message(message):
+            if message.author == client.user:
+                return
+            try:
+                rjson = loads(message.content)
+                tmp = Request(rjson["api_call"], rjson["data"])
+                log.info(f"Received hook data: {tmp}")
+                g["Q"].appendleft(tmp)
+            except Exception as e:
+                err = e
+                log.error(f"Failed to pass message to API: {e}")
+        client.run(g["discord_token"])

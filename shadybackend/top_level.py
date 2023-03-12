007dfc99cd08a02578e463c3684cac5ff824633f
USER 1103 <79700949+user-1103@users.noreply.github.com>
2023-03-12 16:26:44 -0600

    added cli

--- shadybackend/top_level.py
+++ shadybackend/top_level.py
@@ -1,6 +1,7 @@
 """
 Serves as the entry point for runing the SHADY backend.
 """
+from json import loads
 from pathlib import Path
 from signal import SIGINT, signal
 from sys import exit
@@ -11,11 +12,14 @@ from shadybackend.log import log
 from shadybackend.api_tools import G, HookTypes, call_hooks
 from shadybackend.bridges import BRIDGES
 from shadybackend.demon import run_api_demon
+from argparse import ArgumentParser
 
 # Default location for the web tree to be located
 DEFAULT_WEB_ROOT = "./tree"
 # Default location for the api to be collected from
 DEFAULT_API_ROOT = "./api.py"
+# Shady backend version.
+VERSION = 1
 
 
 def run_top_level(start_g: Dict[str, Any], bridge: str,
@@ -68,9 +72,28 @@ signal(SIGINT, on_exit)
 
 
 def run() -> None:
-    with open("keys") as f:
-        token = f.readline()[:-1]
-    run_top_level({"discord_token": token}, "discord")
+    parser = ArgumentParser(
+                    prog='Shady Backend',
+                    description=('CLI interface to run parts of'
+                                 ' the shadybackend through DAD.'),
+                    epilog=f'Release {VERSION}')
+    parser.add_argument("-v", "--verbose", action='store_true',
+                        help="Be chatty.")
+    parser.add_argument("--tree", default=DEFAULT_WEB_ROOT,
+                        help="Provide the location of the web root.")
+    parser.add_argument("--api", default=DEFAULT_API_ROOT,
+                        help="Provide the location of the api.py file.")
+    parser.add_argument("bridge", metavar='B', choices=BRIDGES.keys(),
+                        help="The name of the bridge to use.")
+    parser.add_argument("start_g", metavar='G',
+                        help="A JSON dict to initialize G to.")
+    args = parser.parse_args()
+    if (args.verbose):
+        log.basicConfig(level=log.DEBUG)
+    else:
+        log.basicConfig(level=log.INFO)
+    G = loads(args.start_g)
+    run_top_level(G, args.bridge, args.tree, args.api)
 
 
 if __name__ == "__main__":
